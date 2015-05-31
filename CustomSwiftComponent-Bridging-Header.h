bcad88778e0e49db90e73279e777e967cf358acb
Kevin deLeon <kevin.deleon@gmail.com>
2015-05-30 19:30:33 -0500

    adds file name to comments

--- CustomSwiftComponent-Bridging-Header.h
+++ CustomSwiftComponent-Bridging-Header.h
@@ -1,3 +1,7 @@
+//
+//  CustomSwiftComponent-Bridging-Header.h
+//  CustomSwiftComponent
+
 // This file seems to be neccessary for the module export to work.
 // It's created when we create a Cocoa Touch Class file via the File>New File Menu
 
